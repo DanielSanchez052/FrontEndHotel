0118581ac4c818731acf0b33dc40aa365ac0e044
DanielSanchez052 <sanchezd0528@gmail.com>
2021-11-13 17:02:29 -0500

    initial State

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ReservesComponent } from './reserves/reserves.component';
+import { ReservesFormComponent } from './reserves-form/reserves-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    MainComponent,
+    ReservesComponent,
+    ReservesFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/reserves-form/reserves-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reserves-form',
+  templateUrl: './reserves-form.component.html',
+  styleUrls: ['./reserves-form.component.css']
+})
+export class ReservesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
